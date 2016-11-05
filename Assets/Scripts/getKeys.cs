a35de56facbe699c82f9068cd635a4aaedbe4e3c
Arxcis <jonas.solsvik@gmail.com>
2016-11-05 10:11:02 +0100

    added player movement scripts and a sprite

---
+++ Assets/Scripts/getKeys.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getKeys : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey ("up")) {
+			print ("Up!");
+		}
+
+		if (Input.GetKey ("down")) {
+			print ("down");
+		}
+
+		if (Input.GetKey ("left")) {
+			print ("left");
+		
+		}
+		if (Input.GetKey ("right")) {
+			print ("right");
+		}
+
+
+
+	}
+}
