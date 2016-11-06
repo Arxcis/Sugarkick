1b90239426b3a88103b7430483d4f6ca0f6a1061
JonShard <joneskaara@yahoo.no>
2016-11-06 21:49:39 +0100

    Added Enemy dummy that moves in a line. Its has simple animation system built.[C

---
+++ Assets/Scripts/moveEnemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveEnemy : MonoBehaviour {
+
+    public float moveSpeed = 0.1f;                                  //Movement speed modifier.
+    Transform tfEnemy;
+    Animator aniEnemy;
+    public Transform tfPlayer;                                      //Targets the players tansform.
+    Vector3 vectorToPlayer;
+
+    // Use this for initialization
+    void Start ()
+    {
+        tfEnemy = GetComponent<Transform>();                        //Enemy's transform component.
+        aniEnemy = GetComponent<Animator>();                        //Sprite animator .
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {     
+        //need to put in a line that calculates the vector to the player in a 0-1 perimeter and multiply it by movespeed
+//        vectorToPlayer = new Vector3((tfPlayer.position.x - tfEnemy.position.x), (tfPlayer.position.y - tfEnemy.position.y));
+        
+
+        tfEnemy.position += new Vector3(moveSpeed, 0);              //Moves enemy moveSpeed in x axis.
+        aniEnemy.SetFloat("Speed", Mathf.Abs(moveSpeed));           //Updates animator. So it knows when its moving.
+	}
+}
--- Assets/Scripts/movePlayer.cs
+++ Assets/Scripts/movePlayer.cs
@@ -1,49 +1,4 @@
-<<<<<<< HEAD
-﻿using UnityEngine;
-using System.Collections;
-
-public class movePlayer : MonoBehaviour {
-	//hello Testing
-
-	public static int speed = 6;
-
-	Transform transPlayer;
-	Vector3 UP    = new Vector3(  0, speed );    // Since unity works in 3 dimensions we have to
-	Vector3 DOWN  = new Vector3(  0,-speed );    // create a Vector3 x,y,z, even though we do not
-	Vector3 RIGHT = new Vector3(  speed, 0 );    // care about the z.
-	Vector3 LEFT  = new Vector3( -speed, 0 );
-
-	// Use this for initialization
-	void Start () {
-		transPlayer = GetComponent<Transform>();
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-		if (Input.GetKey ("up")) {
-			print ("up");
-			transPlayer.position += UP * Time.deltaTime;
-		}
-
-		if (Input.GetKey ("down")) {
-			print ("down");
-			transPlayer.position += DOWN * Time.deltaTime;
-		}
-
-		if (Input.GetKey ("left")) {
-			print ("left");
-			transPlayer.position += LEFT * Time.deltaTime;
-		}
-		if (Input.GetKey ("right")) {
-			print ("right");
-			transPlayer.position += RIGHT * Time.deltaTime;
-		}
-
-	}
-}
-=======
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class movePlayer : MonoBehaviour {
@@ -83,4 +38,3 @@ public class movePlayer : MonoBehaviour {
 
 	}
 }
->>>>>>> b1eaa9f4a8187600f4f5838d58e398a8ae60755c
