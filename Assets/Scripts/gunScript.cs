0a05b9265bb120abb64747c8a9bffbbdf7b02f55
JonShard <joneskaara@yahoo.no>
2016-11-15 19:46:24 +0100

    IMPORTANT FIX: There was a fault in the input axis(es) that had been compansated for in the programming of the rotation. Thats was why x was = y, and y was = x. Also added the fireing to the contoller.

--- Assets/Scripts/gunScript.cs
+++ Assets/Scripts/gunScript.cs
@@ -16,6 +16,7 @@ public class gunScript : MonoBehaviour {
     float gunAngle;
     float xAxis = 0.0F;
 	float yAxis = 0.0F;
+    float fire = 0.0F;
     public float rotationSpeed = 10.0F;
 	Vector2 direction = new Vector2(0,0);
 
@@ -39,15 +40,17 @@ public class gunScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update ()
-    {	
-				// GetAxisRawMakes sure that input is not smoothed
-				// Keyboard buttons are either 1 and 0
-				// https://docs.unity3d.com/ScriptReference/Input.GetAxisRaw.html
-		yAxis = Input.GetAxisRaw("AimAxisX");
-		xAxis = Input.GetAxisRaw("AimAxisY");
-		if (yAxis != 0 || xAxis != 0){
-			direction.x = xAxis;
-			direction.y = yAxis;
+    {
+        // GetAxisRawMakes sure that input is not smoothed
+        // Keyboard buttons are either 1 and 0
+        // https://docs.unity3d.com/ScriptReference/Input.GetAxisRaw.html
+        xAxis = Input.GetAxisRaw("AimAxisX");
+        yAxis = Input.GetAxisRaw("AimAxisY");
+        fire = Input.GetAxisRaw("Fire");
+
+        if (xAxis != 0 || yAxis != 0){
+			direction.x = yAxis;
+			direction.y = xAxis;
 
 					// We have left to
 					// Prevent gun from resetting to default position each time the 
@@ -57,10 +60,10 @@ public class gunScript : MonoBehaviour {
 		 	       // Vector2.angle() only outputs positive angles
 					// Depending on the configuration of x and y axis,
 					// in two cases we have to make the gunAngle negative
-			if (xAxis <  0 && yAxis >= 0) {    gunAngle *= -1;     }
-			if (xAxis >= 0 && yAxis >= 0) {    gunAngle *= -1;     }
+			if (yAxis <  0 && xAxis >= 0) {    gunAngle *= -1;     }
+			if (yAxis >= 0 && xAxis >= 0) {    gunAngle *= -1;     }
 
-			Debug.Log("yAxis: " + yAxis + "  xAxis: " + xAxis + "  angle: " + gunAngle + '\n');
+			Debug.Log("xAxis: " + xAxis + "  yAxis: " + yAxis + "  angle: " + gunAngle + '\n');
 					// eulerAngles can be assigned a Vector3
 			gunTrans.localRotation = Quaternion.Euler(0, 0, gunAngle);
 		}
@@ -70,7 +73,7 @@ public class gunScript : MonoBehaviour {
 		}
 
 
-		if (Input.GetButton ("Fire") && cooldown <= 0) {
+		if (fire != 0 && cooldown <= 0) {
 			print ("Firing!"); 
 
 			if (setPos) {
