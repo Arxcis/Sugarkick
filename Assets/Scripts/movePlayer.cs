af915ed547faf3fbe1566f05b73f06c009391e55
JonShard <joneskaara@yahoo.no>
2016-11-15 12:39:50 +0100

    Made movement and aiming binded to contoller.

--- Assets/Scripts/movePlayer.cs
+++ Assets/Scripts/movePlayer.cs
@@ -10,48 +10,30 @@ public float playerSpeed = 0.25f;
     Transform transPlayer;
     Animator animPlayer;
     bool isWalking = false;
-	Vector3 UP    = new Vector3(  0, 1 );    // Since unity works in 3 dimensions we have to
-	Vector3 DOWN  = new Vector3(  0,-1 );    // create a Vector3 x,y,z, even though we do not
-	Vector3 RIGHT = new Vector3(  1, 0 );    // care about the z.
-	Vector3 LEFT  = new Vector3( -1, 0 );
+    Vector3 moveDirVector = new Vector2(0, 0);
+	
 
 	// Use this for initialization
 	void Start () {
 		transPlayer = GetComponent<Transform>();
         animPlayer = GetComponentInChildren<Animator>();
+        
+        
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-        
-            if (Input.GetKey("up"))
-            {
-                print("up");
-                transPlayer.position += UP * playerSpeed;
-            }
+        moveDirVector.x = Input.GetAxisRaw("MoveAxisX");
+        moveDirVector.y = Input.GetAxisRaw("MoveAxisY");
+        transPlayer.position += moveDirVector * playerSpeed;
 
-            else if (Input.GetKey("down"))             //Cant go right while going left(else if)
-            {
-                print("down");
-                transPlayer.position += DOWN * playerSpeed;
-            }
-
-            if (Input.GetKey("left"))
-            {
-                print("left");
-                transPlayer.position += LEFT * playerSpeed;
-            }
-            else if (Input.GetKey("right"))             //Cant go right while going left(else if)
-            {
-                print("right");
-                transPlayer.position += RIGHT * playerSpeed;
-            }
-
-        if (Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right"))
+        if (moveDirVector.x > 0 && moveDirVector.y > 0)
             isWalking = true;                      //updates the animation if the player is walking or...
         else isWalking = false;                     //if the player is not walking.
 
         animPlayer.SetBool("isPlayerWalking", isWalking);
+
+        Debug.Log("yMove: " + moveDirVector.y + "  xMove: " + moveDirVector.x + '\n');
     }
 }
