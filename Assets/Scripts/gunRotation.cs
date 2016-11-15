cbe33420588f2752ec746fae5970620d9e2ae227
Geniejohn <jogaaas@gmail.com>
2016-11-15 11:27:29 +0100

    Fix rotation-script bug

--- Assets/Scripts/gunRotation.cs
+++ Assets/Scripts/gunRotation.cs
@@ -8,7 +8,7 @@ public class gunRotation : MonoBehaviour {
     float gunAngle;
     float xAxis = 0.0F;
 	float yAxis = 0.0F;
-    public float rotationSpeed = 10;
+    public float rotationSpeed = 10.0F;
 	Vector2 direction = new Vector2(0,0);
 
     // Use this for initialization
@@ -25,24 +25,25 @@ public class gunRotation : MonoBehaviour {
 				// https://docs.unity3d.com/ScriptReference/Input.GetAxisRaw.html
 		yAxis = Input.GetAxisRaw("Horizontal");
 		xAxis = Input.GetAxisRaw("Vertical");
-	
-		direction.x = xAxis;
-		direction.y = yAxis;
+		if (yAxis != 0 || xAxis != 0){
+			direction.x = xAxis;
+			direction.y = yAxis;
 
-				// We have left to
-				// Prevent gun from resetting to default position each time the 
-				// player lets of the throttle
-		gunAngle = Vector2.Angle (Vector2.right, direction);
+					// We have left to
+					// Prevent gun from resetting to default position each time the 
+					// player lets of the throttle
+			gunAngle = Vector2.Angle (Vector2.right, direction);
 
-		        // Vector2.angle() only outputs positive angles
-				// Depending on the configuration of x and y axis,
-				// in two cases we have to make the gunAngle negative
-		if (xAxis <  0 && yAxis >= 0) {    gunAngle *= -1;     }
-		if (xAxis >= 0 && yAxis >= 0) {    gunAngle *= -1;     }
+		 	       // Vector2.angle() only outputs positive angles
+					// Depending on the configuration of x and y axis,
+					// in two cases we have to make the gunAngle negative
+			if (xAxis <  0 && yAxis >= 0) {    gunAngle *= -1;     }
+			if (xAxis >= 0 && yAxis >= 0) {    gunAngle *= -1;     }
 
-		Debug.Log("yAxis: " + yAxis + "  xAxis: " + xAxis + "  angle: " + gunAngle + '\n');
-				// eulerAngles can be assigned a Vector3
-		gunTrans.localRotation = Quaternion.Euler(0, 0, gunAngle);
+			Debug.Log("yAxis: " + yAxis + "  xAxis: " + xAxis + "  angle: " + gunAngle + '\n');
+					// eulerAngles can be assigned a Vector3
+			gunTrans.localRotation = Quaternion.Euler(0, 0, gunAngle);
+		}
 
     }
 }
