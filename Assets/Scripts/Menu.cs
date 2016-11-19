4fd32d6e25d085a301161c1848f32da6755d37d4
JonShard <joneskaara@yahoo.no>
2016-11-19 13:16:24 +0100

    Added button click, they are delayed for some reason. I think its because unity loads the music, and adds the buttonclick to the audio stream. So the playe has to catch up before click is played.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -8,10 +8,16 @@ public class Menu : MonoBehaviour {
     public Canvas MainCanvas;                          // Decleares canvases used.
     public Canvas OptionsCanvas;
     public Canvas SelectionCanvas;
+    public AudioClip menuMusic;
+    public AudioClip buttonSound;
+    public float musicVolume = 1f;
+    public float buttonVolume = 1f;
+    AudioSource aud;
 
     void Awake()                                       // Is run before "start" functions.
     {
         mainOpen();
+        aud = GetComponent<AudioSource>();
     }
 
     public void mainOpen()                             // Switches to the level selection.
@@ -19,7 +25,6 @@ public class Menu : MonoBehaviour {
         MainCanvas.enabled      = true;
         SelectionCanvas.enabled = false;
         OptionsCanvas.enabled   = false;
-                             
     }
 
     public void OptionsOpen()                          // Switches back to the main menu.                 
@@ -27,6 +32,8 @@ public class Menu : MonoBehaviour {
         MainCanvas.enabled = false;
         SelectionCanvas.enabled = false;
         OptionsCanvas.enabled = true;
+        aud.PlayOneShot(buttonSound, buttonVolume);
+
     }
 
     public void selectionOpen()                         // Switches to the options menu.
@@ -34,27 +41,34 @@ public class Menu : MonoBehaviour {
         MainCanvas.enabled = false;
         SelectionCanvas.enabled = true;
         OptionsCanvas.enabled = false;
+        aud.PlayOneShot(buttonSound, buttonVolume);
     }
 
     public void LoadOctagon()                           // Loads the different scenes.
     {
+        aud.PlayOneShot(buttonSound, buttonVolume);
         SceneManager.LoadScene("Octagon");
     }
     public void LoadHexagon()
     {
+        aud.PlayOneShot(buttonSound, buttonVolume);
         SceneManager.LoadScene("Hexagon");
     }
     public void LoadSquare()
     {
+        aud.PlayOneShot(buttonSound, buttonVolume);
         SceneManager.LoadScene("Square");
     }
     public void LoadTriangle()
     {
+        aud.PlayOneShot(buttonSound, buttonVolume);
         SceneManager.LoadScene("Triangle");
     }
     public void LoadLine()
     {
+        aud.PlayOneShot(buttonSound, buttonVolume);
         SceneManager.LoadScene("Line");
+        
     }
 
 }
