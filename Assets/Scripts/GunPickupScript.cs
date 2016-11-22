d25e610fd52d1c0995e302f3377f9338eeef04f2
Eldar Hauge Torkelsen <eldar.h.t@hotmail.com>
2016-11-22 09:20:14 +0100

    GunPickupScript for managing multiple weapons

---
+++ Assets/Scripts/GunPickupScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunPickupScript : MonoBehaviour {
+
+    public GameObject[ ] guns; // gun.length should be equal to gunDropChanse.length
+    [Range( 0.0f, 100.0f )]
+    public float[ ] gunDropChanse; //element "n+1" max should be dependent of element "n" max to get a % chanse for that gun to drop.
+
+    // Use this for initializiation
+    void Start( ) {
+
+    }
+
+    // Update is called once per frame
+    void Update( ) {
+
+    }
+}
