6ca8ce6d84a0d5983b610d778751044571cae76f
JonShard <joneskaara@yahoo.no>
2016-11-27 19:35:00 +0100

    wrote an S anre removed it again. needed to commit my changes. LOLZ

--- Assets/Scripts/GunPickupScript.cs
+++ Assets/Scripts/GunPickupScript.cs
@@ -12,4 +12,30 @@ public class GunPickupScript : MonoBehaviour {
 	void Update () {
 		transform.Rotate(new Vector3(0,0,45)*Time.deltaTime);
 	}
-}
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       if (other.gameObject.CompareTag("Player"))
+        {
+            for (int i = 0; i < Main.Player<PuppetManip>(0).guns.Length; ++i)
+            {
+                guns[i].SetActive(false);
+            }
+            switch (other.gameObject.name)
+            {
+                case "GunDrop1":
+                    guns[1].SetActive(true);
+                    break;
+                case "GunDrop2":
+                    guns[2].SetActive(true);
+                    break;
+                case "GunDrop3":
+                    guns[3].SetActive(true);
+                    break;
+            }
+            Destroy(other.gameObject);
+            //GameObject.Find(other.gameObject.name).SetActive(true);
+            //GameObject.GetChild("Gun").gameObject.setActive(false);
+        }
+    }
