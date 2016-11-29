25643a919b1c10bf25a7406efd757bb39b743d6d
Jonas Solsvik <jonas.solsvik@gmail.com>
2016-11-29 16:24:47 +0100

    Adde new files from Eldar

---
+++ Assets/Scripts/EdgeScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EdgeScript : MonoBehaviour {
+    void OnTriggerExit2D(Collider2D other)
+    {
+       if (other.gameObject.CompareTag("Player")){
+
+            PuppetManip mnIp = other.GetComponent<PuppetManip>();   //gets the exited object's puppit manip.
+
+            mnIp.kill("fall");
+        }
+    }
+}
