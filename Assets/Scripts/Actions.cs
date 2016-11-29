b2ced95017058c882227bde2d7bb3c77253895b2
Jonas Solsvik <jonas.solsvik@gmail.com>
2016-11-29 03:22:41 +0100

    #103 Multiplayer GunScript.Fire() works! almost there :)

--- Assets/Scripts/Actions.cs
+++ Assets/Scripts/Actions.cs
@@ -8,11 +8,11 @@ public class Actions : MonoBehaviour {
 	// Public
 	public enum Device : int {		// Used to access correct slot in string[] device names
 		Keyboard1,
-		Keyboard2,
 		Joy1,
 		Joy2,
 		Joy3,
-		Joy4
+		Joy4,
+		Keyboard2
 	};
 
 	public bool keyboardOn = false;													  // On/off keyboard 1 and 2
@@ -21,11 +21,11 @@ public class Actions : MonoBehaviour {
 	// Private
 	string[] DeviceNames = {				// Names that works as keys to access the Input.GetAxis(name+axis)
 		"Keyboard1",
-		"Keyboard2",
 		"Joy1",
 		"Joy2",
 		"Joy3",
 		"Joy4",
+		"Keyboard2"
 	};
 
 	Vector2 moveInput   = Vector2.zero;							// Stores temp values for all the input axes
@@ -41,10 +41,19 @@ public class Actions : MonoBehaviour {
 		MapActiveDevices();           // Sets up each player with an active input device
 	}
 
+
+
 	void FixedUpdate ()
 	{
-		CallActions();							// Sets up all function-calls to their respective input-counterpart
+		for (int i = 0; i < Main.Players().Count; i++)	  	// Wanted to use a foreach here, but player actually never used
+		{
+			CallMove(i);
+			CallAim(i);
+			CallFire(i);
+		}
 	}
+
+
 
 	void MapActiveDevices()
 	{
@@ -55,50 +64,54 @@ public class Actions : MonoBehaviour {
 		}
 	}
 
-	void CallActions()    																	// Actions setup! and execution
+
+	void CallMove( int i )
 	{
-	  for (int i = 0; i < Main.Players().Count; i++)	  	// Wanted to use a foreach here, but player actually never used
-		{
+				// -------------------- Move ------------------------
+				moveInput.x = Input.GetAxisRaw( DeviceNames[ (int)activeDevices[i] ] + "_MoveHorizontal");
+				moveInput.y = Input.GetAxisRaw( DeviceNames[ (int)activeDevices[i] ] + "_MoveVertical"  );
 
-			// -------------------- Move ------------------------
-			moveInput.x = Input.GetAxisRaw( DeviceNames[ (int)activeDevices[i] ] + "_MoveHorizontal");
-			moveInput.y = Input.GetAxisRaw( DeviceNames[ (int)activeDevices[i] ] + "_MoveVertical"  );
+				Main.Player<MovePlayer>(i).Move( moveInput, i);
 
-			Main.Player<MovePlayer>(i).Move( moveInput, i);
+				// Debug.Log( DeviceNames[ (int)activeDevices[i] ] + "_Move" + moveInput);
+	}
 
-			Debug.Log( DeviceNames[ (int)activeDevices[i] ] + "_Move" + moveInput);
+	void CallAim( int i )
+	{
+		// --------------------- Aim --------------------------
+		if( activeDevices[i] == Device.Keyboard1 ) {            				// Mouse aiming
 
-			// --------------------- Aim --------------------------
-			if( activeDevices[i] == Device.Keyboard1 ) {
+			mouseInput = Input.mousePosition;
 
-				mouseInput = Input.mousePosition;
-				// Debug.Log(DeviceNames[ (int)activeDevices[i] ] + "_Mouse" + mouseInput);
-				activeGun = Main.Player<PuppetManip>(i).GetActiveGunScript();
-				if (activeGun != null) {
-					activeGun.MouseAimUpdate(mouseInput, i);
-				}
-
+			activeGun = Main.Player<PuppetManip>(i).GetActiveGunScript();
+			if (activeGun != null) {
+				activeGun.MouseAimUpdate(mouseInput, i);
 			}
-			else
-			{
-			  aimInput.x = Input.GetAxisRaw( DeviceNames[ (int)activeDevices[i] ] + "_AimHorizontal");
-			  aimInput.y = Input.GetAxisRaw( DeviceNames[ (int)activeDevices[i] ] + "_AimVertical"  );
-
-				activeGun = Main.Player<PuppetManip>(i).GetActiveGunScript();
-				if (activeGun != null) {
-					activeGun.KeyAimUpdate(aimInput);
-				}
-				Debug.Log( DeviceNames[ (int)activeDevices[i] ] + "_Aim" + aimInput);
-			}
-
-
-			// --------------------- Fire ------------------------------
-			fireInput = Input.GetAxisRaw( DeviceNames[ (int)activeDevices[i] ] + "_Fire" );
-			// Main.Player<GunScript>(i, "Gun").fire( fireInput );
-
-			if( fireInput > 0.4) Debug.Log( DeviceNames[ (int)activeDevices[i] ] + "_Fire" + fireInput);
-			i++;
+			// Debug.Log(DeviceNames[ (int)activeDevices[i] ] + "_Mouse" + mouseInput);
 		}
+		else																													// Normal aiming
+		{
+			aimInput.x = Input.GetAxisRaw( DeviceNames[ (int)activeDevices[i] ] + "_AimHorizontal");
+			aimInput.y = Input.GetAxisRaw( DeviceNames[ (int)activeDevices[i] ] + "_AimVertical"  );
+
+			activeGun = Main.Player<PuppetManip>(i).GetActiveGunScript();
+			if (activeGun != null) {
+				activeGun.KeyAimUpdate(aimInput);
+			}
+			// Debug.Log( DeviceNames[ (int)activeDevices[i] ] + "_Aim" + aimInput);
+		}
+	}
+
+	void CallFire( int i )
+	{
+		// --------------------- Fire ------------------------------
+		fireInput = Input.GetAxisRaw( DeviceNames[ (int)activeDevices[i] ] + "_Fire" );
+
+		activeGun = Main.Player<PuppetManip>(i).GetActiveGunScript();
+		Debug.Log( DeviceNames[ (int)activeDevices[i] ] + "_Fire" + fireInput);
+		if( fireInput > 0.4)
+			activeGun.Fire(fireInput, i);
+		i++;
 	}
 	// End of class
 }
--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -33,7 +33,6 @@ public class GunScript : MonoBehaviour
     Transform gunTrans;
 
     float gunAngle;                             // Shooting
-    float fire     = 0.0F;
     float cooldown = 0.0F;
 
     // Vector2 inputVec  = new Vector2(0,0);       // Direction input Vector
@@ -41,6 +40,7 @@ public class GunScript : MonoBehaviour
     Vector2 recoilVec = new Vector2(0,0);       // Recoil vector
 
     Vector3 facingMouseVector  = new Vector3(0,0,0);
+
 
     void Start ()
     {
@@ -58,59 +58,66 @@ public class GunScript : MonoBehaviour
           cooldown--;
       }
 
-      fire = Input.GetAxisRaw ("Fire");           // https://docs.unity3d.com/ScriptReference/Input.GetAxisRaw.html
+      // fire = Input.GetAxisRaw ("Fire");           // https://docs.unity3d.com/ScriptReference/Input.GetAxisRaw.html
 
-      if (fire > 0.4 && cooldown <= 0) {      // Scaling recoilVec with negative 1 to send the
-                                              // player in opposite direction
-          recoilVec = Main.GetUnitVector2( (gunAngle+90) * Mathf.Deg2Rad ) * -1;
 
-          if (setPos) {                       // Casting vec2 to vec3 before adding to position
-              Main.Player<Transform>(0).position += Main.ToVector3(recoilVec * (knockbackPow / 300.0F));
-          }
-
-          if(addForce){
-              Main.Player<Rigidbody2D>(0).AddForce(recoilVec * (knockbackPow / 1));
-          }
-
-          if(setVelocity){
-              Main.Player<Rigidbody2D>(0).velocity += recoilVec * (knockbackPow / 10);
-          }
-
-          shoot();
-          cooldown = 1/(fireRate/1000);
-      }
-
-      if (gunAngle > -100 && gunAngle < 110)
-          Main.Player<SpriteRenderer>(0, "Head").sprite = main.headBack;   // non-static part of main has to be called through an object.
-      else
-          Main.Player<SpriteRenderer>(0, "Head").sprite = main.headFront;
   }
+
 
 	// Creating a prefab, "bullets" set in the inspector. Created at the position of "barrelEnd", also set in the inspector. Then the bullet is parented to "bulletParent", also set in the inspector.
 	// The velocity of the bullet is set in the direction of the barrel with a speed of "projectileSpeed", set in the inspector. The bullet spawns with the damage of "weaponDamage".
-	void shoot()
+public void Fire(float fire, int i)
   {
-        float soundPitch = Random.Range(-0.1f, 0.1f);         // the pitch deviation needs to be on a seperate line due to how the compiler deals with functions.
-        SoundManager.instance.bangBang(GunSound, soundPitch); // plays designated gun sound
 
-		float i = Random.Range (-50, 50);  // random float to make the spray deviate a bit.
-		recoilVec = Main.GetUnitVector2( (gunAngle+90+ (i/50)*(100/accuracy)) * Mathf.Deg2Rad )*-1; // (i/50) is a float between -1 and 1 - for a random distribution
-													    // between the extremals.
-													    // (100/accuracy) determines the extremals for the firing diviation.
-													    // The bigger the accuracy, the smaller the deviation.
-		ProjectileInfo pInfo;
-		var bullet = Instantiate (bulletSeed, new Vector3(barrelEnd.GetComponent<Transform>().position.x, barrelEnd.GetComponent<Transform>().position.y, 0), Quaternion.identity) as GameObject;
-		bullet.transform.parent = bulletParent.transform;
-		pInfo = bullet.GetComponent<ProjectileInfo> ();
-		bullet.GetComponent<Rigidbody2D> ().velocity = recoilVec * -1 * projectileSpeed; // Sets the velocity to be a bit random.
-		pInfo.damage = weaponDamage;
-		pInfo.projectileSpeed = projectileSpeed;
-		pInfo.truePiercing = truePiercing;
-		pInfo.pierceNumber = pierceNumber;
-        pInfo.bulletLifeTime = bulletLifeTime;
-        pInfo.bulletSize = bulletSize;
+    // -------------------- PART 1 - EFFECT the player -----------------------
+    if (fire > 0.4 && cooldown <= 0)
+    {                                                   // Scaling recoilVec with negative 1 to send the
+                                                        // player in opposite direction
+      recoilVec = Main.GetUnitVector2( (gunAngle+90) * Mathf.Deg2Rad ) * -1;
 
+      if (setPos) {                                     // Casting vec2 to vec3 before adding to position
+          Main.Player<Transform>(i).position += Main.ToVector3(recoilVec * (knockbackPow / 300.0F));
+      }
+      if(addForce){
+          Main.Player<Rigidbody2D>(i).AddForce(recoilVec * (knockbackPow / 1));
+      }
+      if(setVelocity){
+          Main.Player<Rigidbody2D>(i).velocity += recoilVec * (knockbackPow / 10);
+      }
+
+      // -------------------- PART 2 - EFFECT the bullet - play sound -----------------------
+      float soundPitch = Random.Range(-0.1f, 0.1f);         // the pitch deviation needs to be on a seperate line due to how the compiler deals with functions.
+      SoundManager.instance.bangBang(GunSound, soundPitch); // plays designated gun sound
+
+	    float r = Random.Range (-50, 50);  // random float to make the spray deviate a bit.
+	    recoilVec = Main.GetUnitVector2( (gunAngle+90+ (r/50)*(100/accuracy)) * Mathf.Deg2Rad )*-1; // (i/50) is a float between -1 and 1 - for a random distribution
+	    											    // between the extremals.
+	    											    // (100/accuracy) determines the extremals for the firing diviation.
+	    											    // The bigger the accuracy, the smaller the deviation.
+	    ProjectileInfo pInfo;
+	    var bullet = Instantiate (bulletSeed, new Vector3(barrelEnd.GetComponent<Transform>().position.x, barrelEnd.GetComponent<Transform>().position.y, 0), Quaternion.identity) as GameObject;
+	    bullet.transform.parent = bulletParent.transform;
+	    pInfo = bullet.GetComponent<ProjectileInfo> ();
+	    bullet.GetComponent<Rigidbody2D> ().velocity = recoilVec * -1 * projectileSpeed; // Sets the velocity to be a bit random.
+	    pInfo.damage = weaponDamage;
+	    pInfo.projectileSpeed = projectileSpeed;
+	    pInfo.truePiercing = truePiercing;
+	    pInfo.pierceNumber = pierceNumber;
+      pInfo.bulletLifeTime = bulletLifeTime;
+      pInfo.bulletSize = bulletSize;
+
+
+      cooldown = 1/(fireRate/1000);
+
+      // -------------------- PART 3 - UPDATE HEAD ROTATION -----------------------   // This is a bit malplaced
+      if (gunAngle > -100 && gunAngle < 110)
+          Main.Player<SpriteRenderer>(0, "Head").sprite = main.headBack;      // non-static part of main has to be called through an object.
+      else
+          Main.Player<SpriteRenderer>(0, "Head").sprite = main.headFront;
+    }
 	}
+
+
 
   public void MouseAimUpdate( Vector3 mouseIn, int i )
   {
