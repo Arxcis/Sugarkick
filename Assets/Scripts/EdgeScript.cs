b4ebddfcf73b05ac0f51df7421bc279ea784e278
Jonas Solsvik <jonas.solsvik@gmail.com>
2016-11-30 12:27:23 +0100

    Added isTrigger to PolygonCollider2d on Awake EdgeScript()

--- Assets/Scripts/EdgeScript.cs
+++ Assets/Scripts/EdgeScript.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof(PolygonCollider2D))]
+[RequireComponent (typeof(PolygonCollider2D))]    // Edge detection uses Polygoncollider2d trigger attribute
 public class EdgeScript : MonoBehaviour {
+
+
+    void Awake ()
+    {
+      gameObject.GetComponent<PolygonCollider2D>().isTrigger = true;  // Enable Trigger attribute
+    }
+
     void OnTriggerExit2D(Collider2D other)
     {
        if (other.gameObject.CompareTag("Player")){
 
-            PuppetManip mnIp = other.GetComponent<PuppetManip>();   //gets the exited object's puppit manip.
+            PuppetManip mnIp = other.GetComponent<PuppetManip>();     // Gets the exited object's puppit manip.
 
             mnIp.kill("fall");
         }
